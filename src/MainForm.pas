cf04e91e724ca40dd0762d91c3c6dcf1b96a81ae
Serraniel <serraniel@posteo.de>
2018-09-26 21:22:30 +0200

    Some basic initialization stuff in MainForm

--- src/MainForm.pas
+++ src/MainForm.pas
@@ -3,15 +3,25 @@ unit MainForm;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Bomber4D.GameEngine;
 
 type
   TFrmMain = class(TForm)
+    procedure FormShow(Sender: TObject);
   private
-    { Private-Deklarationen }
+    FGameEngine: TBMGameEngine;
   public
-    { Public-Deklarationen }
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
   end;
 
 var
@@ -20,5 +30,25 @@ var
 implementation
 
 {$R *.dfm}
+{ TFrmMain }
+
+constructor TFrmMain.Create(AOwner: TComponent);
+begin
+  inherited;
+
+  FGameEngine := TBMGameEngine.Create;
+end;
+
+destructor TFrmMain.Destroy;
+begin
+  FreeAndNil(FGameEngine);
+
+  inherited;
+end;
+
+procedure TFrmMain.FormShow(Sender: TObject);
+begin
+  FGameEngine.Init;
+end;
 
 end.
